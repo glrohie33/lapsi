9d148585a375a4f5c994daef0e130772eedc863a
owolabi glory <aremuopomulero@gmail.com>
2021-06-30 18:12:59 +0100

    code split

---
+++ home.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{107:function(n,e,t){"use strict";t.r(e);var o={mounted:function(){console.log("Component mounted.")}},u=t(0),l=Object(u.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div")}),[],!1,null,null,null);e.default=l.exports}}]);
